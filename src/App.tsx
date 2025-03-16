c90667e2f7e451b5ec9766f4df0acfddabc55260
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2025-03-17 04:24:25 +0530

    configure tailwindcss and daisyui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+          <h1 className="text-3xl text-red-300 font-bold underline">
+              Hello world!
+          </h1>
+          <button className="btn btn-primary">Button</button>
+      </>
+  )
+}
+
+export default App
