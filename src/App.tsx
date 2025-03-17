6e623ec12548d1aa2c01a71b8eea3df4b506a278
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2025-03-17 20:50:38 +0530

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import './App.css'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Layout from "./components/Layout.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import Products from "./pages/Products.tsx";
+import Orders from "./pages/Orders.tsx";
 
 function App() {
 
   return (
-      <>
-          <h1 className="text-3xl text-red-300 font-bold underline">
-              Hello world!
-          </h1>
-          <button className="btn btn-primary">Button</button>
-      </>
+      <Router>
+          <Routes>
+              <Route element={<Layout/>}>
+                  <Route path='/' element={<Dashboard/>}/>
+                  <Route path='/products' element={<Products/>}/>
+                  <Route path='/orders' element={<Orders/>}/>
+              </Route>
+          </Routes>
+      </Router>
   )
 }
 
