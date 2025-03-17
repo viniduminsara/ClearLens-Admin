d35301dc218935cbc2d258980364ea13e2777b61
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2025-03-17 20:49:29 +0530

    create header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+
+    return (
+        <footer className="footer footer-center  bg-neutral text-base-content p-4">
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by ClearLens</p>
+            </aside>
+        </footer>
+    )
+}
+
+export default Footer;
