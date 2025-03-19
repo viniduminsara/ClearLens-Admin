9055b28772f5f14943d8a5020573de756bfc7075
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2025-03-19 22:04:44 +0530

    configure product table

---
+++ src/constant/tableCustomStyles.ts
@@ -0,0 +1,36 @@
+export const customStyles = {
+    table: {
+        style: {
+            backgroundColor: "#1D232A",
+            color: "white",
+        },
+    },
+    headRow: {
+        style: {
+            backgroundColor: "#1D232A",
+            color: "#A6ADBB",
+            fontSize: "16px",
+            fontWeight: "bold",
+        },
+    },
+    rows: {
+        style: {
+            backgroundColor: "#1D232A",
+            color: "#A6ADBB",
+            "&:hover": {
+                backgroundColor: "rgb(55, 65, 81)",
+            },
+        },
+    },
+    pagination: {
+        style: {
+            backgroundColor: "#1D232A",
+            color: "#A6ADBB",
+        },
+        pageButtonsStyle: {
+            backgroundColor: "#1D232A",
+            color: "#A6ADBB",
+            fill: "#A6ADBB",
+        }
+    },
+};
---
+++ src/constant/tableHeaders.tsx
@@ -0,0 +1,34 @@
+import { Product } from "../interfaces/user.ts";
+import {Link} from "react-router-dom";
+
+export const productTableColumns = [
+    {
+        name: 'Product Name',
+        selector: (row: Product) => row.name,
+        sortable: true,
+        cell: (row: Product) => <Link to={`/product/${row._id}`}>{row.name}</Link>,
+    },
+    {
+        name: 'Image',
+        selector: (row: Product) => row.image,
+        cell: (row: Product) => <img src={row.image} className="w-24 h-18" alt="image" />,
+    },
+    {
+        name: 'Stock',
+        selector: (row: Product) => row.quantity,
+        sortable: true,
+        cell: (row: Product) => <span>{row.quantity}</span>,
+    },
+    {
+        name: 'Price',
+        selector: (row: Product) => row.price,
+        sortable: true,
+        cell: (row: Product) => <span>{row.price}</span>,
+    },
+    {
+        name: 'New Price',
+        selector: (row: Product) => row.newPrice,
+        sortable: true,
+        cell: (row: Product) => <span>{row.newPrice}</span>,
+    },
+];
